033a9dc2725b3b4d347fb29190da4b69080b043f
drandreaskrueger <andreas.krueger@electron.org.uk>
2017-01-16 16:07:29 +0000

    test parity before geth before eth

--- bin/eth.sh
+++ bin/eth.sh
@@ -16,7 +16,17 @@ fi
 echo "Local IP: $LOCALIP"
 echo "Public IP: $IP"
 
-if [[ -f $(which geth 2>/dev/null) ]]
+# TODO: check before if already running:
+# curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"web3_clientVersion","id":1}' http://127.0.0.1:8545
+
+if [[ -f $(which parity 2>/dev/null) ]]
+then
+	echo "Starting parity"
+	CMD="parity --bootnodes enode://09fbeec0d047e9a37e63f60f8618aa9df0e49271f3fadb2c070dc09e2099b95827b63a8b837c6fd01d0802d457dd83e3bd48bd3e6509f8209ed90dabbc30e3d3@52.16.188.185:30303 --nat \"extip:$IP\""
+	echo $CMD
+	$CMD
+
+elif [[ -f $(which geth 2>/dev/null) ]]
 then
 	echo "Starting geth"
 	echo geth --rpc --bootnodes "enode://09fbeec0d047e9a37e63f60f8618aa9df0e49271f3fadb2c070dc09e2099b95827b63a8b837c6fd01d0802d457dd83e3bd48bd3e6509f8209ed90dabbc30e3d3@52.16.188.185:30303" --nat "extip:$IP"
